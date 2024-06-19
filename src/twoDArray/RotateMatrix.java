3c84cdb1f7e0f84447daf1416d0dd046113a95a0
Bikashkumar <bikashbikash756@gmail.com>
2024-06-19 07:34:38 +0530

    pascal traingle and rotatematrix

---
+++ src/twoDArray/RotateMatrix.java
@@ -0,0 +1,2 @@
+package twoDArray;public class RotateMatrix {
+}
--- src/twoDArray/transposeMatrix.java
+++ src/twoDArray/transposeMatrix.java
@@ -1,2 +1,53 @@
-package twoDArray;public class transposeMatrix {
+package twoDArray;
+
+import java.util.Scanner;
+
+public class transposeMatrix {
+
+    static void printMatrix(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    static int[][] transposeMatrix(int[][] matrix, int rows, int cols) {
+        int[][] transposed = new int[cols][rows];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                transposed[j][i] = matrix[i][j];
+            }
+        }
+        return transposed;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        try {
+            System.out.println("Enter number of rows:");
+            int rows = sc.nextInt();
+            System.out.println("Enter number of columns:");
+            int cols = sc.nextInt();
+            int[][] matrix = new int[rows][cols];
+
+            System.out.println("Enter the matrix values:");
+            for (int i = 0; i < rows; i++) {
+                for (int j = 0; j < cols; j++) {
+                    matrix[i][j] = sc.nextInt();
+                }
+            }
+
+            System.out.println("Original Matrix:");
+            printMatrix(matrix);
+
+            int[][] transposedMatrix = transposeMatrix(matrix, rows, cols);
+            System.out.println("Transposed Matrix:");
+            printMatrix(transposedMatrix);
+        }
+        finally {
+            sc.close();
+        }
+    }
 }
